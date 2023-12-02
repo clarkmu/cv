99b4dbc22f31bd95ece6b7befb84513cf955c566
Michael <clarkmu@gmail.com>
2023-12-02 00:51:09 -0500

    try csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     siteUrl: `https://clarkmu.com`,
     title: "Michael Clark Portfolio",
   },
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+      },
+    },
+  ],
 };
