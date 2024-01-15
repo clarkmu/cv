20c50f7910e0b68f06b14924a7dc15bb02d1220a
Michael <clarkmu@gmail.com>
2024-01-15 10:54:03 -0500

    can collapse menu

--- src/components/fabric/GravitySimulator.jsx
+++ src/components/fabric/GravitySimulator.jsx
@@ -199,42 +199,66 @@ export default function GravitySimulator() {
     }
   }, [gravityCount, particleCount]);
 
-  const Menu = () => (
-    //   user menu overlay component
-    <div className="absolute top-2 left-2 flex flex-col gap-4 bg-[rgba(255,255,255,0.25)] p-4 rounded">
-      <InputContainer label="Gravities">
-        <Input
-          type="number"
-          value={gravityCount}
-          onChange={(e) => setGravityCount(validateNumericInput(e))}
-          min={0}
-        />
-      </InputContainer>
-      <InputContainer label="Particles">
-        <Input
-          type="number"
-          value={particleCount}
-          onChange={(e) => setParticleCount(validateNumericInput(e))}
-          min={0}
-        />
-      </InputContainer>
-      <InputContainer label="Speed">
-        <Input
-          type="number"
-          value={speed}
-          onChange={(e) => setSpeed(validateNumericInput(e))}
-          min={1}
-          step={speed > 101 ? 100 : 10}
-        />
-      </InputContainer>
-      <button
-        className="bg-white hover:bg-gray-100 rounded px-2 py-1"
-        onClick={() => setRegenerate(Math.random())}
-      >
-        Regenerate
-      </button>
-    </div>
-  );
+  const Menu = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    return (
+      //   user menu overlay component
+
+      <div className="w-[15ch] absolute top-2 left-2 flex flex-col gap-4 bg-[rgba(255,255,255,0.25)] p-4 rounded select-none">
+        <div
+          className="bg-[rgba(255,255,255,0.5)] text-center cursor-pointer"
+          onClick={() => setCollapsed((b) => !b)}
+        >
+          <div
+            className={
+              " transition-all rotate-0 " + (collapsed && " !-rotate-90")
+            }
+          >
+            V
+          </div>
+        </div>
+        {!collapsed && (
+          <>
+            <InputContainer label="Gravities">
+              <Input
+                type="number"
+                value={gravityCount}
+                onChange={(e) => setGravityCount(validateNumericInput(e))}
+                min={0}
+              />
+            </InputContainer>
+            <InputContainer label="Particles">
+              <Input
+                type="number"
+                value={particleCount}
+                onChange={(e) => setParticleCount(validateNumericInput(e))}
+                min={0}
+              />
+            </InputContainer>
+            <InputContainer label="Speed">
+              <Input
+                type="number"
+                value={speed}
+                onChange={(e) => setSpeed(validateNumericInput(e))}
+                min={1}
+                step={speed > 101 ? 100 : 10}
+              />
+            </InputContainer>
+            <button
+              className="bg-white hover:bg-gray-100 rounded px-2 py-1"
+              onClick={() => setRegenerate(Math.random())}
+            >
+              Regenerate
+            </button>
+            <span className="bg-white text-black text-sm px-2 py-1">
+              Pan and zoom to move around.
+            </span>
+          </>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="bg-black w-full h-full relative">
