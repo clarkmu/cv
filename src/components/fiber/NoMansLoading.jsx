7c4566f0de50fb35bfe54af3bfa1b31dac8e016e
Michael <clarkmu@gmail.com>
2024-01-21 13:11:35 -0500

    fix NMSLoading.message position lg

--- src/components/fiber/NoMansLoading.jsx
+++ src/components/fiber/NoMansLoading.jsx
@@ -83,7 +83,7 @@ export default function NoMansLoading() {
             ></div>
           );
         })}
-        <div className="absolute w-[50vw] -translate-x-1/2 bg-[rgba(255,255,255,0.95)] text-center text-xl font-bolder rounded translate-y-[-300%]">
+        <div className="absolute w-[50vw] -translate-x-1/2 bg-[rgba(255,255,255,0.95)] text-center text-xl font-bolder rounded translate-y-[-300%] lg:translate-y-[-600%]">
           Coming soon. For now, here is the No Man's Sky loading screen.
         </div>
       </div>
